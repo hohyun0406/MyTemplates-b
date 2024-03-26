6ca5b18f388c6d07ff11d9688c1f068297bdffd7
hohyun0406 <hohyun0406@gmail.com>
2024-03-26 20:13:29 +0900

    .

--- springboot-3/src/main/java/com/rod/api/article/Article.java
+++ springboot-3/src/main/java/com/rod/api/article/Article.java
@@ -1,2 +1,28 @@
-package com.rod.api.article;public class Article {
+package com.rod.api.article;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "articles")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString(exclude = {"id"})
+public class Article {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String content;
+    private String writer;
+    private String registerDate;
+
+    @Builder(builderMethodName = "builder")
+    public Article(long id, String title, String content, String writer, String registerDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.registerDate = registerDate;
+    }
 }
