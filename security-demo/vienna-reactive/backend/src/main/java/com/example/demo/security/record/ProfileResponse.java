6cbdb1fea7d0708dcbf93c4ae7298953de907ddc
hohyun0406 <hohyun0406@gmail.com>
2024-06-11 21:27:26 +0900

    240611 reactive demo

--- security-demo/vienna-reactive/backend/src/main/java/com/example/demo/security/record/ProfileResponse.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.security.record;
-
-import java.util.Set;
-
-record ProfileResponse(String username, Set<String> roles) {
-}
--- security-demo/vienna-reactive/frontend/src/app/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-	return <div className="mt-auto flex justify-around items-center text-sm py-2">
-    <span>&copy; {new Date().getFullYear()} Designly</span>
-  </div>;
-}
